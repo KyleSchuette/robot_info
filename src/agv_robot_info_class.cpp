2a22aaf57a69cba4e0986d45cb4ea042e340c8c2
Kyle Schuette <kschuette2008@gmail.com>
2025-10-14 22:53:02 +0000

    Updated AGVRobotInfo Class output

--- src/agv_robot_info_class.cpp
+++ src/agv_robot_info_class.cpp
@@ -1,4 +1,5 @@
 #include "robot_info/robot_info.h"
+#include "robotinfo_msgs/RobotInfo10Fields.h"
 
 class AGVRobotInfo : public RobotInfo {
 public:
@@ -8,20 +9,28 @@ public:
 
   void publish_data() override {
     robotinfo_msgs::RobotInfo10Fields msg;
-    msg.data_field_01 = robot_description + " (AGV)";
-    msg.data_field_02 = serial_number;
-    msg.data_field_03 = ip_address;
-    msg.data_field_04 = firmware_version;
-    msg.data_field_05 = std::to_string(maximum_payload) + " kg";
+
+    msg.data_field_01 = "robot_description: " + robot_description;
+    msg.data_field_02 = "serial_number: " + serial_number;
+    msg.data_field_03 = "ip_address: " + ip_address;
+    msg.data_field_04 = "firmware_version: " + firmware_version;
+    msg.data_field_05 = "maximum_payload: " + std::to_string(maximum_payload) + " Kg";
+
+    // Fill remaining fields empty
+    msg.data_field_06 = "";
+    msg.data_field_07 = "";
+    msg.data_field_08 = "";
+    msg.data_field_09 = "";
+    msg.data_field_10 = "";
 
     info_pub_.publish(msg);
 
-    ROS_INFO_STREAM("[AGVRobotInfo] Published AGV info:"
-                    << " Description=" << robot_description
-                    << " | Serial=" << serial_number
-                    << " | IP=" << ip_address
-                    << " | Firmware=" << firmware_version
-                    << " | Payload=" << maximum_payload << " kg");
+    ROS_INFO_STREAM("[AGVRobotInfo] Published: "
+                    << msg.data_field_01 << " | "
+                    << msg.data_field_02 << " | "
+                    << msg.data_field_03 << " | "
+                    << msg.data_field_04 << " | "
+                    << msg.data_field_05);
   }
 };
 
@@ -29,11 +38,11 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "agv_robot_info_node");
 
   AGVRobotInfo agv;
-  agv.robot_description = "RB1 AGV";
-  agv.serial_number = "RB1-AGV-007";
-  agv.ip_address = "192.168.0.42";
-  agv.firmware_version = "v3.0.0";
-  agv.maximum_payload = 120.0f;
+  agv.robot_description = "Cooper100";
+  agv.serial_number = "567A359";
+  agv.ip_address = "169.254.5.180";
+  agv.firmware_version = "3.5.8";
+  agv.maximum_payload = 100.0f;
 
   ros::Rate rate(1);
   while (ros::ok()) {
