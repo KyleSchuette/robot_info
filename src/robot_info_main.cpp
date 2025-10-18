70207bac3d6ea7717e3ba82f6a3e3a9e30ea9284
Kyle Schuette <kschuette2008@gmail.com>
2025-10-18 17:56:05 +0000

    Adjusted member variable values to match requirements

--- src/robot_info_main.cpp
+++ src/robot_info_main.cpp
@@ -4,10 +4,10 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "robot_info_node");
 
   RobotInfo base_robot;
-  base_robot.robot_description = "RB1 General Model";
-  base_robot.serial_number = "RB1-2025-001";
-  base_robot.ip_address = "192.168.1.42";
-  base_robot.firmware_version = "v2.3.7";
+  base_robot.robot_description = "Cooper100";
+  base_robot.serial_number = "567A359";
+  base_robot.ip_address = "169.254.5.180";
+  base_robot.firmware_version = "3.5.8";
 
   ros::Rate rate(1);
   while (ros::ok()) {
@@ -15,6 +15,6 @@ int main(int argc, char **argv) {
     ros::spinOnce();
     rate.sleep();
   }
-
+  
   return 0;
 }
